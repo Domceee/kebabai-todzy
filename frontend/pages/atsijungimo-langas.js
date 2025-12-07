2d42c8184573bc4775f5735582fb74f41f7ac994
rutagaiz <rutagaiz@gmail.com>
2025-12-07 02:18:14 +0200

    Atsijungimo funkcija

--- frontend/pages/atsijungimo-langas.js
+++ frontend/pages/atsijungimo-langas.js
@@ -1,12 +1,27 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export default function AtsijungimoLangas() {
+
+  const router = useRouter()
+
+  const handleLogout = () => {
+    // 1. Ištrinti naudotojo sesiją
+    localStorage.removeItem("user")
+
+    // 2. Nukreipti į pagrindinį langą
+    router.push("/")
+  }
+
   return (
     <main style={{ padding: 20 }}>
       <h1>Atsijungimo langas</h1>
       <p>Ar tikrai norite atsijungti?</p>
       
-      <button style={{ padding: '10px 20px', marginTop: 10, cursor: 'pointer' }}>
+      <button 
+        onClick={handleLogout}
+        style={{ padding: '10px 20px', marginTop: 10, cursor: 'pointer' }}>
         Atsijungti
       </button>
       
